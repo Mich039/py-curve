4de1b4f4e36c3ebecf112a0ddbbdf0dd5f50a22a
Michael Heckmann <s1810307074@students.fh-hagenberg.at>
2021-02-09 17:15:34 +0100

    Fixed Typo in GameState

--- GameObjects/GameState.py
+++ GameObjects/GameState.py
@@ -8,7 +8,7 @@ from GameObjects.PowerUp import PowerUp
 class GameState:
 
     def __init__(self):
-        self._player_listList[Player] = []
+        self._player_list: List[Player] = []
         self._to_remove: List[int] = []
         self._ground_power_up = None
         self._state = None
