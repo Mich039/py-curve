41329412daa68bf6231b7a3ba8293c64d21f448e
Michael Heckmann <s1810307074@students.fh-hagenberg.at>
2021-02-24 22:37:37 +0100

    Added additional PowerUpTypes

--- Client/ClientConstants.py
+++ Client/ClientConstants.py
@@ -15,5 +15,9 @@ POWER_UP_RADIUS = 15
 POWER_UP_COLORS = {
     PowerUpType.SLOW: (255, 50, 50),
     PowerUpType.SPEED: (50, 255, 50),
-    PowerUpType.FLYING: (50, 50, 255)
+    PowerUpType.FLYING: (50, 50, 255),
+    PowerUpType.CLEAR: (50, 50, 255),
+    PowerUpType.ENEMY_INVERSE: (50, 50, 255),
+    PowerUpType.CORNER: (50, 50, 255),
+    PowerUpType.ENEMY_CORNER: (50, 50, 255)
 }
